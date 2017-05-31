2552023a08977a30dcfacc2686ff933c9c0a818a
ReeceFarmerHGA <Reece.Farmer@hgacreative.com>
2017-05-31 11:29:46 +0100

    Trying blank html5 theme with skeleton

---
+++ wp-content/themes/html5blank-stable/index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<main role="main" class="six columns">
+			<section>
+				<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+				<?php get_template_part('loop'); ?>
+				<?php get_template_part('pagination'); ?>
+			</section>
+		</main>
+
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
