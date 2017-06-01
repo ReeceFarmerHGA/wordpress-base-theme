72cf397ebf8dc69842aa3529d3013ff7a4a78ba7
ReeceFarmerHGA <Reece.Farmer@hgacreative.com>
2017-06-01 11:52:39 +0100

    Added container classes where necessary

--- wp-content/themes/hgabase/archive.php
+++ wp-content/themes/hgabase/archive.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<div class="container">
+		<div class="row">
+			<main role="main" class="md8">
+				<!-- section -->
+				<section>
+					<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
+					<?php get_template_part('loop'); ?>
+					<?php get_template_part('pagination'); ?>
+				</section>
+				<!-- /section -->
+			</main>
+			<?php get_sidebar(); ?>
+		</div><!-- row -->
+	</div><!-- container -->
 
 <?php get_footer(); ?>
--- wp-content/themes/hgabase/search.php
+++ wp-content/themes/hgabase/search.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<div class="container">
+		<div class="row">
+			<main role="main" class="md8">
+				<!-- section -->
+				<section>
+					<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+					<?php get_template_part('loop'); ?>
+					<?php get_template_part('pagination'); ?>
+				</section>
+				<!-- /section -->
+			</main>
+			<?php get_sidebar(); ?>
+		</div><!-- row -->
+	</div><!-- container -->
 
 <?php get_footer(); ?>
