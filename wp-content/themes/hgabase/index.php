d61423753b7f733d27da8b0a0795213dfd542218
ReeceFarmerHGA <Reece.Farmer@hgacreative.com>
2017-06-01 11:41:12 +0100

    More variables and code block:

--- wp-content/themes/hgabase/index.php
+++ wp-content/themes/hgabase/index.php
@@ -2,7 +2,7 @@
 
 <div class="container">
 	<div class="row">
-		<main role="main" class="sm6">
+		<main role="main" class="md8">
 			<section class="posts-list">
 				<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 				<?php get_template_part('loop'); ?>
@@ -12,6 +12,14 @@
 					<?php get_template_part('pagination'); ?>
 				</section>
 			<?php endif; ?>
+			<section>
+				<h1>Heading 1</h1>
+				<h2>Heading 2</h2>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
+			</section>
 			<section>
 				<form>
 					<div class="row">
@@ -40,26 +48,76 @@
 				</form>
 			</section>
 			<section>
+				<p>
+					<p>Primary</p>
+					<input class="btn btn--primary" type="submit" value="Submit">
+					<div class="alert alert--primary">This is a primary alert</div>
+				</p>
 
-				<input class="btn btn--primary" type="submit" value="Submit">
-				<div class="alert alert--primary">This is a primary alert</div>
-				<p>Primary</p>
+				<p>
+					<p>Secondary</p>
+					<input class="btn btn--secondary" type="submit" value="Submit">
+					<div class="alert alert--secondary">This is a secondary alert</div>
+				</p>
 
-				<input class="btn btn--secondary" type="submit" value="Submit">
-				<div class="alert alert--secondary">This is a secondary alert</div>
-				<p>Secondary</p>
+				<p>
+					<p>Success</p>
+					<input class="btn btn--success" type="submit" value="Submit">
+					<div class="alert alert--success">This is a success alert</div>
+				</p>
 
-				<input class="btn btn--success" type="submit" value="Submit">
-				<div class="alert alert--success">This is a success alert</div>
-				<p>Success</p>
+				<p>
+					<p>Warning</p>
+					<input class="btn btn--warning" type="submit" value="Submit">
+					<div class="alert alert--warning">This is a warning alert</div>
+				</p>
 
-				<input class="btn btn--warning" type="submit" value="Submit">
-				<div class="alert alert--warning">This is a warning alert</div>
-				<p>Warning</p>
-
-				<input class="btn btn--error" type="submit" value="Submit">
-				<div class="alert alert--error">This is an error alert</div>
-				<p>Error</p>
+				<p>
+					<p>Error</p>
+					<input class="btn btn--error" type="submit" value="Submit">
+					<div class="alert alert--error">This is an error alert</div>
+				</p>
+			</section>
+			<section>
+				<table class="u-full-width">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Age</th>
+							<th>Sex</th>
+							<th>Location</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Reece Farmer</td>
+							<td><?php echo date(Y) - 1997 ?></td>
+							<td>Male</td>
+							<td>Carlisle</td>
+						</tr>
+						<tr>
+							<td>Ian Austin</td>
+							<td><?php echo date(Y) - 1992 ?></td>
+							<td>Male</td>
+							<td>Lancaster</td>
+						</tr>
+						<tr>
+							<td>Elliot Garner</td>
+							<td><?php echo date(Y) - 1993 ?></td>
+							<td>Male</td>
+							<td>Manchester</td>
+						</tr>
+					</tbody>
+				</table>
+			</section>
+			<section>
+				<pre>
+					<code>
+.some-class {
+	background-color: plum;
+}
+					</code>
+				</pre>
 			</section>
 		</main>
 
